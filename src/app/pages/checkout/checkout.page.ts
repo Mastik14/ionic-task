4e4e4baa8386903af1f5db76b3f2c6abe5504716
Sergey <www.sergeypygach@mail.ru>
2019-10-25 17:41:06 +0300

    finish form validation

--- src/app/pages/checkout/checkout.page.ts
+++ src/app/pages/checkout/checkout.page.ts
@@ -5,6 +5,8 @@ import { ReviewService } from '../../services/review.service';
 import { Review } from '../../models/review.model';
 import { Answer } from '../../models/answer.model';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'checkout',
   templateUrl: './checkout.page.html',
@@ -14,11 +16,61 @@ export class CheckoutPage implements OnInit {
   loadedAnswers: Answer[];
   loadedReviews: Review[];
 
+  form: FormGroup;
+
   constructor(private answersService: AnswersService, private reviewService: ReviewService) { }
 
   ngOnInit() {
     this.loadedAnswers = this.answersService.answers;
     this.loadedReviews = this.reviewService.reviews;
+    this.form = new FormGroup({
+      firstName: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(2), Validators.maxLength(20)]
+      }),
+      lastName: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(2), Validators.maxLength(20)]
+      }),
+      email: new FormControl('', {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.email]
+      }),
+      phone: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(8)]
+      }),
+      address: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
+      city: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
+      unitedStates: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
+      state: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
+      zip: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.minLength(6)]
+      }),
+    });
+  }
+
+  submit() {
+
+    if (this.form.valid) {
+      console.log('Form: ', this.form);
+      const formData = {...this.form.value};
+
+      console.log('Form Data:', formData);
+    }
   }
 
 }
